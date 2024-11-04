904b9783135b59814649e962b97811828e43ec15
Brent Kerby <blkerby@gmail.com>
2024-11-03 20:31:09 -0700

    fix bad clear tiles

--- src/fix_clear_tiles.py
+++ src/fix_clear_tiles.py
@@ -4,32 +4,40 @@ import glob
 
 path_glob = "Projects/*/Export/Rooms/*.xml"
 
+def fix_screen(screen):
+    word_list = screen.text.split()
+    rows = []
+    modified_cnt = 0
+    for y in range(16):
+        cell_list = []
+        for x in range(16):
+            i = y * 16 + x
+            tile = int(word_list[i], 16)
+            gfx = tile & 0x3FF
+            if 0x8E <= gfx <= 0x95:
+                new_tile = 0xFF
+                modified_cnt += 1
+            else:
+                new_tile = tile
+
+            cell = "{:04X}".format(new_tile)
+            cell_list.append(cell)
+        rows.append("        " + " ".join(cell_list))
+    data = "\n" + "\n".join(rows) + "\n      "
+    screen.text = data
+    return modified_cnt
+
 for room_path in glob.glob(path_glob):
     tree = xml.parse(room_path)
     modified_cnt = 0
     states = tree.findall("./States/State")
     for state_node in states:
-        layer2_screens = state_node.findall("./LevelData/Layer2/Screen")
-        for layer2_screen in layer2_screens:
-            layer2_list = layer2_screen.text.split()
-            rows = []
-            for y in range(16):
-                cell_list = []
-                for x in range(16):
-                    i = y * 16 + x
-                    layer2_tile = int(layer2_list[i], 16)
-                    layer2_gfx = layer2_tile & 0x3FF
-                    if 0x8E <= layer2_gfx <= 0x95:
-                        new_tile = 0xFF
-                        modified_cnt += 1
-                    else:
-                        new_tile = layer2_tile
+        for screen in state_node.findall("./LevelData/Layer2/Screen"):
+            modified_cnt += fix_screen(screen)
+        if "CERES" not in room_path:
+            for screen in state_node.findall("./LevelData/Layer1/Screen"):
+                modified_cnt += fix_screen(screen)
 
-                    cell = "{:04X}".format(new_tile)
-                    cell_list.append(cell)
-                rows.append("        " + " ".join(cell_list))
-            data = "\n" + "\n".join(rows) + "\n      "
-            layer2_screen.text = data
     if modified_cnt > 0:
         print("Fixed {} errors in {}".format(modified_cnt, room_path))
         tree.write(room_path)
